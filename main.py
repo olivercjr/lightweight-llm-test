d21b0291dad9ee45908eb04ff086911fd54b3188
Oliver Tinashe <97441294+olivercjr@users.noreply.github.com>
2025-11-18 13:34:46 +0100

    Refactor chatbot function for improved chat formatting and structure

--- main.py
+++ main.py
@@ -11,30 +11,34 @@ def chatbot():
 	print("\n---CHAT STARTS HERE---")
 	greeting = os.getenv("GREETING")
 	print(f"{os.getenv("BOT_NAME")}: {greeting}")
-	chat_history = f"<|assistant|>\n{greeting}<|assistant_end|>"
+	chat_history = f"\n<start_of_turn>model\n{greeting}\n<end_of_turn>\n"
+	pretty_chat = f"Assistant: {greeting}"
 
 
 	# Ask questions
 	while True:
 		user_msg = input(f"\n{os.getenv("USER_NAME")}: ")
 		if user_msg == "/stop": break
-		else: chat_history += (f"\n<|user|>\n{user_msg}<|user_end|>\n<|assistant|>\n")
+		else:
+			chat_history += (f"\n<start_of_turn>user\n{user_msg}\n<end_of_turn>\n\n<start_of_turn>model\n")
+			pretty_chat += f"\nUser: {user_msg}"
 		print("--------------")
 		
-		# Chat formatting
+		# Gemma 3 Chat formatting
 		system_prompt = os.getenv("SYSTEM_PROMPT")
-		full_prompt = f"<|system|>\n{system_prompt}<|system_end|>\n{chat_history}"
-		response = llm(prompt=full_prompt, max_tokens=200, stop=["<|assistant_end|>", "<|user_end|>", "<|system_end|>"],temperature=0.6)
-		# response = llm(prompt=full_prompt, max_tokens=200, temperature=0.6)
+		full_prompt = f"<start_of_turn>system\n{system_prompt}\n<end_of_turn>\n{chat_history}"
+		response = llm(prompt=full_prompt, max_tokens=200, stop=["<end_of_turn>"], temperature=0.6)
 		response_str = response['choices'][0]['text']
-		chat_history += f"{response_str}<|assistant_end|>"
+		chat_history += f"{response_str}\n<end_of_turn>\n" #
+		pretty_chat += f"\nAssistant: {response_str}"
 
 		# Print the model's response
 		print(f"\n{os.getenv("BOT_NAME")}: {response_str}")
 
 	# end program
 	print("\n---CHAT ENDS HERE---")
-	print(f"<|system|>\n{system_prompt}<|system_end|>\n{chat_history}")
+	# print(f"###SYSTEM PROMPT###\n{system_prompt}\n###########\n{pretty_chat}")
+	print(full_prompt)
 
 
 
